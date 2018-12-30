06f8c7c2437c8cbdb4e8690611a72921f4f4b769
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2018-12-31 02:38:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const WebSocket = require('ws');
 express = require('express');
 https = require('https');
 request = require('request');
-var path = require('path')
+var path = require('path');
+var app = express();
 var prt = process.env.PORT || 8080;
 const wss = new WebSocket.Server({ port: prt });
 
