4a220fabd234ec61f7b6c138c8f1cd2aa38ae859
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2018-12-30 19:27:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var port = process.env.port || 8080;
 const wss = new WebSocket.Server({ port: port });
  
 wss.on('connection', function connection(wss) {
+ console.log('new connection');
   wss.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
