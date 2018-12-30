53e9f5a8965b36c56322617675e76e549ffd5af3
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2018-12-31 02:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ request = require('request');
 var path = require('path');
 var app = express();
 var prt = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port: prt });
-
-wss.on('connection', function connection(ws) {
-  console.log("connected...");
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
 app.post('/webhook', function (req, res) {
   console.log(res.queryResult);
 res.sendStatus(200);
