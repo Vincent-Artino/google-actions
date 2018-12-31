e4f69c71837cc2d337fcd5c5bd6107ab657c5a46
Vincent Artino <Vincent-Artino@users.noreply.github.com>
2018-12-31 15:21:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 express = require('express');
+https = require('https');
 http = require('http');
 request = require('request');
 var path = require('path');
@@ -12,7 +13,7 @@ const server = http.createServer(app);
 // create websocket server and listen
 const wss = new WebSocket.Server({ server });
 // on connection events
-/*wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws) {
   console.log("connected...");
 // handle message events  
   ws.on('message', function incoming(message) {
@@ -21,14 +22,13 @@ const wss = new WebSocket.Server({ server });
 //handle response
   ws.send('something');
 });
-*/
 // create a home page for the website
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 // handle post requests
 app.post('/webhook', function (req, res) {
-  console.log(req.body);
+  console.log(req);
 res.sendStatus(200);
 });
 // lister to port 
